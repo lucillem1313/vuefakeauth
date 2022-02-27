fe979119edb6f7775056c71a896c234d95e496a4
lucillem1313 <https://github.com/lucillem1313>
2022-02-27 14:49:54 -0600

    secret page locic

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,29 +1,38 @@
 
 <script setup>
-
+import { useRouter } from "vue-router";
 import useAuth from "../composable/useAuth";
 const { isAuthenticated, logout } = useAuth();
+
+const router = useRouter();
+const loggingOut = () => {
+  logout();
+  router.push("/");
+};
 
 </script>
 
 <template>
 <div class="bg-purple-400 text-indigo-500">
-<div class="container mx-auto  justify-center py-4">  
+<div class="container flex items-center  justify-between mx-auto">  
 
-<h1 class=" text-center tracking-tighter text-5xl font-thick">
+<h1 class=" tracking-tighter text-5xl font-thick">
 Vue<span class="font-semibold text-gray-600">FakeAuth</span>
 </h1>
 <nav>
-<ul class="space-x-6"><router-link to="/index">
+<ul class="flex space-x-4"><router-link to="/">
 
-<li class="bg-gray-400 text-lg py-2 px-2 hover:cursor-pointer text-center hover:bg-indigo-300 hover:text-orange-500"> Home</li></router-link> 
-<router-link to="/About"><li class="bg-gray-400 tx-lg py-2 px-2 hover:cursor-pointer text-center hover:bg-indigo-300 hover:text-orange-500"> About</li></router-link> 
-<router-link v-if="!isAuthenticated" to="/Login"><li class="bg-gray-400 tx-lg py-2 px-2 hover:cursor-pointer text-center hover:bg-indigo-300 hover:text-red-500"> Login</li></router-link> 
+<li class="py-8 px-4 hover:cursor-pointer  hover:bg-indigo-300 hover:text-orange-500"> Home</li></router-link> 
+<router-link to="/About"><li class="px-4 py-8 hover:cursor-pointer hover:bg-indigo-300 hover:text-orange-500"> About</li></router-link> 
+<router-link v-if="!isAuthenticated" to="/Login"><li class="bg-gray-400 px-4 py-8 hover:cursor-pointer hover:bg-indigo-300 hover:text-red-500"> Login</li></router-link> 
+<div v-else class="flex">
+<router-link  to="/Secret"><li class="px-4 py-8 hover:cursor-pointer hover:bg-indigo-300 hover:text-red-500"> Secret</li></router-link> 
 
-<router-link v-else to="/Secret"><li class="bg-gray-400 tx-lg py-2 px-2 hover:cursor-pointer text-center hover:bg-indigo-300 hover:text-red-500"> Secret</li></router-link> 
-<button v-else @click="logout">
-  <li class="bg-gray-400 tx-lg py-2 px-2 hover:cursor-pointer text-center hover:bg-indigo-300 hover:text-red-500"> Logout</li></button> 
+<button  @click="loggingOut">
+<li class=" px-6 py-8 hover:cursor-pointer hover:bg-indigo-300 hover:text-red-500"> Logout</li></button>
 
+
+</div>
 </ul>
 
 
